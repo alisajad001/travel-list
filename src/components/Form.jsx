911f9645d4dad79ff8fae26d573732237be74b46
alisajad001 <forwebonly0@gmail.com>
2024-01-22 23:54:05 +0430

    Add: Form inputs & Render number array for quantity

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,17 @@
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
